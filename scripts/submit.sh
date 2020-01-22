525527855c31966f16ed49523d0742b7b4e8a698
Jose <jose.benitez@cern.ch>
2020-01-22 17:03:31 +0100

    submit

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,7 +1,5 @@
 
 export INPUT=root://cms-xrd-global.cern.ch//store/relval/CMSSW_10_6_0_patch2/RelValNuGun/GEN-SIM-RECO/PU25ns_106X_upgrade2023_realistic_v3_2023D42PU0p5-v1/10000/28749F73-4D48-374E-800E-D70FB8C37FC6.root
 export OUTPUT=/eos/user/b/benitezj/BRIL/UpgradeStudies/CMSSW_10_6_0_patch2-PU25ns_106X_upgrade2023_realistic_v3_2023D42PU0p5-v1/summary_PU0p5.root
-cmsRun BRIL_ITsim/ITclusterAnalyzer/python/ITclusterAnalyzer_cfg.py
-
-
+cmsRun BRIL-upgrade/ITclusterAnalyzer_cfg.py
 
