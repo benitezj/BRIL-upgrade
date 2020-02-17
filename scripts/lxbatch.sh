003a4fec9ed1afcea6969129a68051e49218ed7a
Jose <jose.benitez@cern.ch>
2020-02-17 20:32:15 +0100

    lxbatch

--- scripts/lxbatch.sh
+++ scripts/lxbatch.sh
@@ -3,49 +3,37 @@
 ## directory which contains info about the samples and where log files will be put
 submitdir=$1
 if [ "$submitdir" == "" ]; then
-echo "invalid submitdir"
-return
+    echo "invalid submitdir"
+    return
 fi
 
 
 ## what to do : 0=create scripts, 1=submit, 2=check
 action=$2
-if [ "$action" == "" ]; then
-echo "invalid action"
-return
-fi
 
-
-outputdir=/eos/user/b/benitezj/BRIL/
+##where the results will be stored, this directory will be created below
+outputdir=/eos/user/b/benitezj/BRIL/UpgradeStudies/$submitdir
 echo "output: $outputdir"
 
-
 #get the absolute path
-submitdir=`readlink -f $submitdir`
-
 INSTALLATION=${CMSSW_BASE}/src
+cfg=$INSTALLATION/BRIL-upgrade/ITclusterAnalyzer_cfg.py
+fullsubmitdir=`readlink -f $submitdir`
 
-
-
+echo "Config: $cfg"
 
 ########################
 ### lxbatch submit
 submit(){
     local job=$1
-    rm -f $submitdir/${job}.log
+    rm -f $fullsubmitdir/${job}.log
     rm -f ${outputdir}/${job}.root
-    condor_submit $submitdir/${job}.sub 
+    condor_submit $fullsubmitdir/${job}.sub 
 }
 
 ## copy the cfg
 if [ "$action" == "0" ]; then 
-    cfg=$3
-    if [ "$cfg" == "" ]; then
-	echo "No cfg provided\n"
-	exit;
-    fi
-
-    /bin/cp $cfg $submitdir/cfg.py
+    /bin/cp $cfg $fullsubmitdir/cfg.py
 
     echo "mkdir -p $outputdir"
     mkdir -p $outputdir
@@ -55,39 +43,43 @@ fi
 
 ##### loop over the jobs
 counter=0
-for f in `/bin/ls $submitdir | grep .txt | grep -v "~" `; do
+for f in `/bin/ls $fullsubmitdir | grep .txt | grep -v "~" `; do
 
     job=`echo $f | awk -F".txt" '{print $1}'`
+    echo $job
 
     ###create the scripts
     if [ "$action" == "0" ]; then
-	rm -f $submitdir/${job}.sh
-	rm -f $submitdir/${job}.sub
+	rm -f $fullsubmitdir/${job}.sh
+	rm -f $fullsubmitdir/${job}.sub
 
 	##create the CMSSW job
-	echo "export X509_USER_PROXY=${HOME}/x509up_u55361 " >> $submitdir/${job}.sh
-	echo "cd ${INSTALLATION} " >> $submitdir/${job}.sh
-	echo "eval \`scramv1 jobtime -sh\` " >> $submitdir/${job}.sh
-	echo "cd \$TMPDIR  "   >> $submitdir/${job}.sh
-	echo "pwd  "   >> $submitdir/${job}.sh
+	echo "export X509_USER_PROXY=${HOME}/x509up_u55361 " >> $fullsubmitdir/${job}.sh
+	echo "cd ${INSTALLATION} " >> $fullsubmitdir/${job}.sh
+	echo "eval \`scramv1 jobtime -sh\` " >> $fullsubmitdir/${job}.sh
+	echo "cd \$TMPDIR  "   >> $fullsubmitdir/${job}.sh
+	echo "pwd  "   >> $fullsubmitdir/${job}.sh
 
-	echo "export INPUTFILE=${submitdir}/${job}.txt" >> $submitdir/${job}.sh
+	echo "export INPUTFILE=${fullsubmitdir}/${job}.txt" >> $fullsubmitdir/${job}.sh
 
-	echo "env" >> $submitdir/${job}.sh
-	echo "cmsJob  ${submitdir}/cfg.py" >> $submitdir/${job}.sh
-	echo "cp CorrectionHisto.root $outputdir/${job}.root " >> $submitdir/${job}.sh
+	echo "env" >> $fullsubmitdir/${job}.sh
+
+	echo "Hello Ashish. This is job ${job}" >> $fullsubmitdir/${job}.sh
+
+	#echo "cmsJob  ${fullsubmitdir}/cfg.py" >> $fullsubmitdir/${job}.sh
+	#echo "cp CorrectionHisto.root $outputdir/${job}.root " >> $fullsubmitdir/${job}.sh
 
 
 	##create condor jdl
-	echo "Universe   = vanilla" >>  $submitdir/${job}.sub
-	echo "+JobFlavour = \"workday\" " >> $submitdir/${job}.sub
-#	echo "+JobFlavour = \"testmatch\" " >> $submitdir/${job}.sub
-	echo "Executable = /bin/bash" >> $submitdir/${job}.sub 
-	echo "Arguments  = ${submitdir}/${job}.sh" >> $submitdir/${job}.sub 
-	echo "Log        = ${submitdir}/${job}.log" >> $submitdir/${job}.sub 
-	echo "Output     = ${submitdir}/${job}.log" >> $submitdir/${job}.sub 
-	echo "Error      = ${submitdir}/${job}.log" >> $submitdir/${job}.sub 
-	echo "Queue  " >> $submitdir/${job}.sub 
+	echo "Universe   = vanilla" >>  $fullsubmitdir/${job}.sub
+	echo "+JobFlavour = \"workday\" " >> $fullsubmitdir/${job}.sub
+#	echo "+JobFlavour = \"testmatch\" " >> $fullsubmitdir/${job}.sub
+	echo "Executable = /bin/bash" >> $fullsubmitdir/${job}.sub 
+	echo "Arguments  = ${fullsubmitdir}/${job}.sh" >> $fullsubmitdir/${job}.sub 
+	echo "Log        = ${fullsubmitdir}/${job}.log" >> $fullsubmitdir/${job}.sub 
+	echo "Output     = ${fullsubmitdir}/${job}.log" >> $fullsubmitdir/${job}.sub 
+	echo "Error      = ${fullsubmitdir}/${job}.log" >> $fullsubmitdir/${job}.sub 
+	echo "Queue  " >> $fullsubmitdir/${job}.sub 
 
     fi
     
@@ -102,4 +94,3 @@ for f in `/bin/ls $submitdir | grep .txt | grep -v "~" `; do
 done
 
 echo "Total jobs: $counter"
-
