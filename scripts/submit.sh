adf1b8dfea5d47abc3b66fc5b9e5ce286ad5f77f
Ashish <asehrawat1991@gmail.com>
2020-07-25 14:11:42 -0700

    script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -5,7 +5,7 @@
 TINKU=""
 COUNT=0
 file="/afs/cern.ch/user/a/asehrawa/CMSSW_10_6_0_patch2/src/BRIL-upgrade/samples/2023D42PU200.txt"
-while  IFS= read -r line && [ $COUNT -lt 25 ]; do
+while  IFS= read -r line && [ $COUNT -lt 50 ]; do
 #echo "$COUNT"
 cft=root://cms-xrd-global.cern.ch/     
 COUNT=$(($COUNT +1))
