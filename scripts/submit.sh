0a12c790ae5631fce58d1449285370653da8a0a4
Ashish <asehrawat1991@gmail.com>
2020-07-25 14:09:50 -0700

    script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -2,20 +2,20 @@
 
 #INPUTPATH=root://cms-xrd-global.cern.ch/    
                                                                                                                                   
-SONORA=""
+TINKU=""
 COUNT=0
-file="/afs/cern.ch/user/a/asehrawa/CMSSW_10_6_0_patch2/src/BRIL-upgrade/samples/2023D42noPU.txt"
+file="/afs/cern.ch/user/a/asehrawa/CMSSW_10_6_0_patch2/src/BRIL-upgrade/samples/2023D42PU200.txt"
 while  IFS= read -r line && [ $COUNT -lt 25 ]; do
 #echo "$COUNT"
 cft=root://cms-xrd-global.cern.ch/     
 COUNT=$(($COUNT +1))
 line=$cft$line
-SONORA=$SONORA$line,
+TINKU=$TINKU$line,
 done < "$file"
 echo "Value of count is: $COUNT"
-echo "${SONORA::-1}" 
-export INPUT=${SONORA::-1}
+echo "${TINKU::-1}" 
+export INPUT=${TINKU::-1}
 OUTPUTPATH=/eos/user/a/asehrawa/TEPX
-#export OUTPUT=$OUTPUTPATH/summary_PU0.root
+#export OUTPUT=$OUTPUTPATH/summary_PU200.root
 
 cmsRun  /afs/cern.ch/user/a/asehrawa/CMSSW_10_6_0_patch2/src/BRIL-upgrade/ITclusterAnalyzer_cfg.py
